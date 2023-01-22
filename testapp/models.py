e0765670e35e5d9e5b432be70582cb2637185ea3
Sahil Pandere <pandere.sahil23@gmail.com>
2023-01-22 13:49:46 +0530

    first commit

---
+++ testapp/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.urls import reverse
+
+# Create your models here.
+
+class Product(models.Model):
+    ProductId=models.IntegerField()
+    ProductName=models.CharField(max_length=30)
+    CategoryId=models.IntegerField()
+    CategoryName=models.CharField(max_length=30)
+
+    def get_absolute_url(self):
+        return reverse('details',kwargs={'pk':self.pk})
